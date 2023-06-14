3d483dbe42c515bc5247a37271649aa4bbb646f6
Daisel10 <davidsanchezlombana10@gmail.com>
2023-06-14 14:43:31 -0500

    finish

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+
+let PORT = process.env.PORT || 3000
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,9 +26,32 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+  const { date } = req.params;
+
+  let timestamp;
+  if (!date) {
+    // Si no se proporciona ninguna fecha, devuelve la fecha y hora actual
+    timestamp = new Date();
+  } else {
+    // Intenta analizar la fecha proporcionada
+    timestamp = new Date(date);
+  }
+
+  if (isNaN(timestamp.getTime())) {
+    // Si la fecha es inválida, devuelve un objeto de error
+    res.json({ error: 'Invalid Date' });
+  } else {
+    // Construye la respuesta con el timestamp en formato Unix y UTC
+    res.json({
+      unix: timestamp.getTime(),
+      utc: timestamp.toUTCString()
+    });
+  }
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
